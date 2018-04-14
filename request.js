4a3e2674098bf4f68992dc08f68bbc998045716c
Matthew Querzoli <matthewquerzoli@Matts-MacBook-Pro.local>
2018-04-14 13:10:50 +1000

    First commit

---
+++ request.js
@@ -0,0 +1,48 @@
+console.log('\n');
+console.log('Where is it five o\'clock? I need to justify this beer.');
+console.log('\n');
+console.log('Use the following as your excuse, you alcoholic.');
+console.log('\n');
+
+var countryNames = [];
+var timeZones = [];
+var unixTimes = [];
+var times = [];
+
+var request = require('request');
+request('http://api.timezonedb.com/v2/list-time-zone?key=W9VXG3ZAZ6ZD&format=json', function (error, response, body) {
+  var jsonArray = JSON.parse(body);
+  var arr = jsonArray.zones;
+  extractCountriesAndTimes(arr);
+  convertTimes();
+  logValues();
+  console.log('\n');
+});
+
+function extractCountriesAndTimes(responseBody) {
+  responseBody.forEach(function(element) {
+    countryNames.push(element.countryName);
+    timeZones.push(element.zoneName);
+    unixTimes.push(element.timestamp);
+  });
+}
+
+function convertTimes() {
+  unixTimes.forEach(function(element) {
+    times.push(getTimeFromUnixTime(element));
+  });
+}
+
+function getTimeFromUnixTime(unixTime) {
+  var date = new Date(unixTime*1000);
+  var hours = date.getHours();
+  return hours;
+}
+
+function logValues() {
+  for (var i = 0; i < countryNames.length; i++) {
+    if (times[i] == 5) {
+      console.log(countryNames[i] + ' - ' + timeZones[i] + ' - ' + times[i]);
+    }
+  }
+}
